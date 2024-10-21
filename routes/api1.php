4ed556bc5863ebf4af0274f9a72232e89b24bb9a
Oscar Ortiz <oscarm.ortizf@gmail.com>
2024-10-20 19:42:45 -0500

    Delete routes/api1.php

--- routes/api1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CompetitionController;
-
-Route::get('/competitions/leaderboard', [CompetitionController::class, 'leaderboard']);
-Route::post('/competitions/athlete/{id}/start', [CompetitionController::class, 'start']);
-Route::put('/competitions/athlete/{id}/finish', [CompetitionController::class, 'finish']);
